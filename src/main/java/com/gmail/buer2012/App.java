8cc75ee84167b8239446d6f405d81d217ec745d0
nbukrieiev <nikita.bukrieiev@cengage.com>
2020-01-21 17:08:44 +0200

    Refactoring

--- src/main/java/com/gmail/buer2012/App.java
+++ src/main/java/com/gmail/buer2012/App.java
@@ -2,56 +2,45 @@ package com.gmail.buer2012;
 
 import com.gmail.buer2012.bot.TelegramBot;
 import com.gmail.buer2012.entity.Task;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-import javax.persistence.Query;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class App {
-
-    private static final List<String> chatIds = Arrays.asList("304022315", "397052865");
     
-    public static void main(String[] args) throws TelegramApiException, InterruptedException {
+    private static final List<Long> chatIds = Arrays.asList(304022315L, 397052865L);
+    
+    public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.addAnnotatedClass(Task.class);
         SessionFactory sessionFactory = configuration.configure().buildSessionFactory();
-
+        
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        TelegramBot bot = new TelegramBot(sessionFactory.getCurrentSession());
+        
+        TelegramBot bot = new TelegramBot(sessionFactory);
         try {
             botsApi.registerBot(bot);
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
         
-        Random rand = new Random();
-        while (true) {
-            List<Task> tasks = getTasks(sessionFactory.getCurrentSession());
-            Task randomTask = tasks.get(rand.nextInt(tasks.size()));
-            bot.setCurrentTask(randomTask);
-            for(String chat: chatIds) {
-                bot.execute(new SendMessage(chat, randomTask.getRussianWord()));
-            }
-            Thread.sleep(10 * 60 * 1000);
-        }
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(() -> {
+            bot.setTasks();
+            bot.setNewTask();
+            chatIds.forEach(bot::sendTask);
+        }, 1L, 30L, TimeUnit.SECONDS);
     }
     
-    public static List<Task> getTasks(Session session) {
-        session.beginTransaction();
-        Query query = session.createQuery("from Task");
-        List<Task> tasks = query.getResultList();
-        session.getTransaction().commit();
-        return tasks;
-    }
+    
 }
--- src/main/java/com/gmail/buer2012/bot/TelegramBot.java
+++ src/main/java/com/gmail/buer2012/bot/TelegramBot.java
@@ -1,8 +1,9 @@
 package com.gmail.buer2012.bot;
 
-import com.gmail.buer2012.App;
 import com.gmail.buer2012.entity.Task;
+import com.google.common.collect.ImmutableList;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,62 +12,109 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import javax.persistence.Query;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TelegramBot extends TelegramLongPollingBot {
-
+    
     private static String BOT_TOKEN = "980204452:AAGaHipzq3CNsGiFdQTR-Fd0lU_PdSe2pgo";
     private static String BOT_USERNAME = "BukerEnglish_bot";
-    private static String DELETE = "Delete";
-    private static String NEXT = "Next";
+    private static List<String> DELETE_STRINGS = ImmutableList.of("Delete", "delete");
+    private static List<String> NEXT_STRINGS = ImmutableList.of("Next", "next");
     private static String SUCCESS = "Удалено";
-    private Session session;
+    
+    private List<Long> chatIds;
+    private SessionFactory sessionFactory;
+    private List<Task> tasks;
     private Task currentTask;
-
-    public TelegramBot(Session session) {
-        this.session = session;
+    private Random random;
+    
+    public TelegramBot(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        this.random = new Random(System.currentTimeMillis());
     }
-
+    
     @Override
     public void onUpdateReceived(Update update) {
         if ((update.hasMessage() && update.getMessage().hasText()) || update.hasCallbackQuery()) {
-            try {
-                if (update.getMessage().getText().equals(DELETE)) {
-                    this.session.beginTransaction();
-                    Query qry = this.session.createQuery("delete from Task where Task.id = (:id)");
-                    qry.setParameter("id", currentTask.getId());
-                    List<Task> tasks = App.getTasks(this.session);
-                    Random rand = new Random();
-                    Task randomTask = tasks.get(rand.nextInt(tasks.size()));
-                    this.setCurrentTask(randomTask);
-                    this.execute(new SendMessage(update.getMessage().getChatId(), SUCCESS));
-                    this.execute(new SendMessage(update.getMessage().getChatId(), randomTask.getRussianWord()));
-                    this.session.getTransaction().commit();
-                } else if (update.getMessage().getText().equals(NEXT)) {
-                    this.session.beginTransaction();
-                    List<Task> tasks = App.getTasks(this.session);
-                    Random rand = new Random();
-                    Task randomTask = tasks.get(rand.nextInt(tasks.size()));
-                    this.setCurrentTask(randomTask);
-                    this.execute(new SendMessage(update.getMessage().getChatId(), randomTask.getRussianWord()));
-                    this.session.getTransaction().commit();
-                } else {
-                    execute(new SendMessage(update.getMessage().getChatId(), currentTask.getEnglishWord()));
-                }
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if (DELETE_STRINGS.contains(update.getMessage().getText())) {
+                deleteTask();
+                sendMessage(update.getMessage().getChatId(), SUCCESS);
+            } else if (NEXT_STRINGS.contains(update.getMessage().getText())) {
+                sendRandomTask(update.getMessage().getChatId());
+            } else {
+                sendAnswer(update.getMessage().getChatId());
             }
         }
     }
-
-    public void setCurrentTask(Task currentTask) {
-        this.currentTask = currentTask;
+    
+    public void sendTask(Long chatId) {
+        sendMessage(chatId, currentTask.getRussianWord());
     }
-
+    
+    
+    public void setNewTask() {
+        currentTask = tasks.get(random.nextInt(tasks.size()));
+    }
+    
+    public void setTasks() {
+        Session session = getSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Task");
+        List<Task> tasks = query.getResultList();
+        session.getTransaction().commit();
+        this.tasks = tasks;
+        session.close();
+    }
+    
+    public void deleteTask() {
+        Session session = getSession();
+        session.beginTransaction();
+        Query query = session.createQuery("delete from Task where Task.id = (:id)");
+        query.setParameter("id", currentTask.getId());
+        query.executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+    private Session getSession() {
+        return sessionFactory.openSession();
+    }
+    
+    private void sendAnswer(Long chatId) {
+        sendMessage(chatId, currentTask.getEnglishWord());
+    }
+    
+    private void sendRandomTask(Long chatId) {
+        Task randomTask = tasks.get(random.nextInt(tasks.size()));
+        sendMessage(chatId, randomTask.getRussianWord());
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(() -> sendMessage(chatId, randomTask.getEnglishWord()), 10L, TimeUnit.SECONDS);
+    }
+    
+    private void sendMessage(Long chatId, String body) {
+        try {
+            execute(new SendMessage(chatId, body));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Long> getChatIds() {
+        return chatIds;
+    }
+    
+    public void setChatIds(List<Long> chatIds) {
+        this.chatIds = chatIds;
+    }
+    
     @Override
     public String getBotUsername() {
         return BOT_USERNAME;
     }
-
+    
     @Override
     public String getBotToken() {
         return BOT_TOKEN;
